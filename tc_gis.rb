6df9cc4129d8ba6b3da9cccfb1cf96a129d3e1b5
Micah Stalberg <stalberm@oregonstate.edu>
2022-12-06 09:57:14 -0800

    Changed multiline string tests to each cover single concept

--- tc_gis.rb
+++ tc_gis.rb
@@ -44,36 +44,53 @@ class TestGis < Test::Unit::TestCase
     assert_equal(result, expected)
   end
 
-  def test_tracks
+  def test_multilinestring_multiple_linestrings
+
     ls_coords = [
       Coordinate.new(-122, 45),
       Coordinate.new(-122, 46),
       Coordinate.new(-121, 46),
     ]
-
     ls1 = LineString.new(ls_coords)
     
     ls_coords = [
       Coordinate.new(-121, 45),
       Coordinate.new(-121, 46),
     ]
-
     ls2 = LineString.new(ls_coords)
-
-    ls_coords = [
-      Coordinate.new(-121, 45.5),
-      Coordinate.new(-122, 45.5),
-    ]
-    
-    ls3 = LineString.new(ls_coords)
 
     mls = MultiLineString.new([ls1, ls2], "track 1")
     expected = JSON.parse('{"type": "Feature", "properties": {"title": "track 1"},"geometry": {"type": "MultiLineString","coordinates": [[[-122,45],[-122,46],[-121,46]],[[-121,45],[-121,46]]]}}')
     result = JSON.parse(mls.get_json)
     assert_equal(expected, result)
+  end
 
-    mls = MultiLineString.new([ls3], "track 2")
-    expected = JSON.parse('{"type": "Feature", "properties": {"title": "track 2"},"geometry": {"type": "MultiLineString","coordinates": [[[-121,45.5],[-122,45.5]]]}}')
+  def test_multilinestring_one_linestring
+
+    ls_coords = [
+      Coordinate.new(-122, 45),
+      Coordinate.new(-122, 46),
+      Coordinate.new(-121, 46),
+    ]
+    ls1 = LineString.new(ls_coords)
+
+    mls = MultiLineString.new([ls1], "track 1")
+    expected = JSON.parse('{"type": "Feature", "properties": {"title": "track 1"},"geometry": {"type": "MultiLineString","coordinates": [[[-122,45],[-122,46],[-121,46]]]}}')
+    result = JSON.parse(mls.get_json)
+    assert_equal(expected, result)
+  end
+ 
+  def test_multilinestring_missing_name
+
+    ls_coords = [
+      Coordinate.new(-122, 45),
+      Coordinate.new(-122, 46),
+      Coordinate.new(-121, 46),
+    ]
+    ls1 = LineString.new(ls_coords)
+
+    mls = MultiLineString.new([ls1])
+    expected = JSON.parse('{"type": "Feature", "properties": {},"geometry": {"type": "MultiLineString","coordinates": [[[-122,45],[-122,46],[-121,46]]]}}')
     result = JSON.parse(mls.get_json)
     assert_equal(expected, result)
   end
